cc8cb30b03d9bb40b2a64e2a95347cff0ec41a9a
Pranav EK <pranavek@users.noreply.github.com>
2019-04-28 12:19:43 +0530

    Update SecondLargestNumber.py

--- Python/SecondLargestNumber.py
+++ Python/SecondLargestNumber.py
@@ -1,12 +1,4 @@
-
-# coding: utf-8
-
-# In[1]:
-
 input_array = [2,3,5,8,1,9]
-
-
-# In[3]:
 
 first = second = 0
 for i in input_array[:]:
@@ -16,13 +8,7 @@ for i in input_array[:]:
     elif i > second and i != first:
         second = i
 
-
-# In[4]:
-
 print("The largest number is {} and the second largest number is {}".format(first, second))
-
-
-# In[ ]:
 
 
 
