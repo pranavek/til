5dd22be33cdd33d868373ed44e9e35e39a60f811
pranavek <kni8fdhel@gmail.com>
2020-04-25 12:31:44 +0530

    Testing the GCD

--- Java/Algorithms_Robert_Sedgewick/EuclidsAlgorithm.java
+++ Java/Algorithms_Robert_Sedgewick/EuclidsAlgorithm.java
@@ -3,6 +3,10 @@ class EuclidsAlgorithm{
         System.out.println(new EuclidsAlgorithm().gcd(5,10));
     }
 
+    /**
+     * Use recursion to divide the divisor by the reminder until reminder becomes zero. 
+     * Return the the divisor when the reminder is zero
+     */
     int gcd(int a, int b){
         if(b ==0) return a;
         int r = a % b;
